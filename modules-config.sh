e4c20f6f5151e6809b2e86f66de01fe85292888b
Constantin Runge <c.runge@cssbook.de>
2017-03-21 20:28:12 +0100

    streamline module system

--- modules-config.sh
+++ modules-config.sh
@@ -1,33 +1,37 @@
 
-module_bash_from=shells/bash/
-module_bash_link=(.bash_logout .bash_profile .bashrc)
-module_bash_depend=(shell)
+user_module_bash_from=shells/bash/
+user_module_bash_link=(bash_logout bash_profile bashrc)
+user_module_bash_depend=(shell)
 
-module_git_link=(.gitconfig)
+user_module_git_link=(gitconfig)
 
-module_i3_from=x/
-module_i3_link=(.i3)
-module_i3_depend=(x)
+user_module_i3_from=x/
+user_module_i3_link=(i3)
+user_module_i3_depend=(x)
 
-module_nvim_link=(.config/nvim)
+user_module_nvim_to=.config
+user_module_nvim_link=(nvim)
+user_module_nvim_options=(noprefix)
 
-module_shell_from=shells/
-module_shell_link=(.shellrc)
+user_module_shell_from=shells/
+user_module_shell_link=(shellrc)
 
-module_ssh_copy=(.ssh)
+user_module_ssh_copy=(ssh)
 
-module_vim_link=(.vim .vimrc .gvimrc)
+user_module_vim_link=(vim vimrc gvimrc)
 
-module_x_from=x/
-module_x_link=(.xinitrc .Xresources .xprofile)
+user_module_x_from=x/
+user_module_x_link=(xinitrc Xresources xprofile)
 
-module_xscreensaver_from=x/
-module_xscreensaver_link=(.xscreensaver)
-module_xscreensaver_depend=(x)
+user_module_xscreensaver_from=x/
+user_module_xscreensaver_link=(xscreensaver)
+user_module_xscreensaver_depend=(x)
 
-module_xdg_link=(.config/user-dirs.dirs)
+user_module_xdg_to=.config
+user_module_xdg_link=(user-dirs.dirs)
+user_module_xdg_options=(noprefix)
 
-module_zsh_from=shells/
-module_zsh_link=(.zshrc)
-module_zsh_depend=(shell)
+user_module_zsh_from=shells/
+user_module_zsh_link=(zshrc)
+user_module_zsh_depend=(shell)
 
