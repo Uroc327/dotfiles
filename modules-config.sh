570abad953d5bb0279d3d28c2493dd906ac70ee5
Constantin Runge <c.runge@cssbook.de>
2017-03-21 19:51:17 +0100

    add modules config

---
+++ modules-config.sh
@@ -0,0 +1,29 @@
+
+module_bash_from=shells/bash/
+module_bash_link=(.bash_logout .bash_profile .bashrc)
+module_bash_depend=(shell)
+
+module_git_link=(.gitconfig)
+
+module_i3_from=x/
+module_i3_link=(.i3)
+module_i3_depend=(x)
+
+module_nvim_link=(.config/nvim)
+
+module_shell_from=shells/
+module_shell_link=(.shellrc)
+
+module_ssh_copy=(.ssh)
+
+module_vim_link=(.vim .vimrc .gvimrc)
+
+module_x_from=x/
+module_x_link=(.xinitrc .Xresources .xprofile .xscreensaver)
+
+module_xdg_link=(.config/user-dirs.dirs)
+
+module_zsh_from=shells/
+module_zsh_link=(.zshrc)
+module_zsh_depend=(shell)
+
