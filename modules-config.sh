a6a75d2107ef20e42e256c388dca131448cc8334
Constantin Runge <c.runge@cssbook.de>
2018-03-01 21:26:11 +0100

    playing around with config format

--- modules-config.sh
+++ modules-config.sh
@@ -1,42 +1,41 @@
 
 repo_name=crunge
-repo_crunge_type=user
 
-crunge_module_bash_from=shells/bash/
-crunge_module_bash_link=(bash_logout bash_profile bashrc)
-crunge_module_bash_depend=(shell)
+user_module[crunge.bash.from]=shells/bash/
+user_module[crunge.bash.link]=(bash_logout bash_profile bashrc)
+user_module[crunge.bash.depend]=(shell)
 
-crunge_module_git_link=(gitconfig)
-#crunge_module_git_template=(gitconfig)
+#user_module[crunge.git.link]=(gitconfig)
+user_module[crunge.git.template]=(gitconfig)
 
-crunge_module_i3_from=x/
-crunge_module_i3_link=(i3)
-crunge_module_i3_depend=(x)
+user_module[crunge.i3.from]=x/
+user_module[crunge.i3.link]=(i3)
+user_module[crunge.i3.depend]=(x)
 
-crunge_module_nvim_to=.config
-crunge_module_nvim_link=(nvim)
-crunge_module_nvim_options=(noprefix)
+user_module[crunge.nvim.to]=.config
+user_module[crunge.nvim.link]=(nvim)
+user_module[crunge.nvim.options]=(noprefix)
 
-crunge_module_oni_link=(oni)
-crunge_module_oni_depend=(nvim)
+user_module[crunge.oni.link]=(oni)
+user_module[crunge.oni.depend]=(nvim)
 
-crunge_module_shell_from=shells/
-crunge_module_shell_link=(shellrc)
+user_module[crunge.shell.from]=shells/
+user_module[crunge.shell.link]=(shellrc)
 
-crunge_module_vim_link=(vim vimrc gvimrc)
+user_module[crunge.vim.link]=(vim vimrc gvimrc)
 
-crunge_module_x_from=x/
-crunge_module_x_link=(xinitrc Xresources xprofile)
+user_module[crunge.x.from]=x/
+user_module[crunge.x.link]=(xinitrc Xresources xprofile)
 
-crunge_module_xscreensaver_from=x/
-crunge_module_xscreensaver_link=(xscreensaver)
-crunge_module_xscreensaver_depend=(x)
+user_module[crunge.xscreensaver.from]=x/
+user_module[crunge.xscreensaver.link]=(xscreensaver)
+user_module[crunge.xscreensaver.depend]=(x)
 
-crunge_module_xdg_to=.config
-crunge_module_xdg_link=(user-dirs.dirs)
-crunge_module_xdg_options=(noprefix)
+user_module[crunge.xdg.to]=.config
+user_module[crunge.xdg.link]=(user-dirs.dirs)
+user_module[crunge.xdg.options]=(noprefix)
 
-crunge_module_zsh_from=shells/
-crunge_module_zsh_link=(zshrc)
-crunge_module_zsh_depend=(shell)
+user_module[crunge.zsh.from]=shells/
+user_module[crunge.zsh.link]=(zshrc)
+user_module[crunge.zsh.depend]=(shell)
 
