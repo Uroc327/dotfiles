c81693ea0058436d7d0aedd90532f36ef336792d
Constantin Runge <c.runge@cssbook.de>
2018-03-01 17:58:10 +0100

    play a bit with modules-config.sh

--- modules-config.sh
+++ modules-config.sh
@@ -1,39 +1,42 @@
 
-user_module_bash_from=shells/bash/
-user_module_bash_link=(bash_logout bash_profile bashrc)
-user_module_bash_depend=(shell)
+repo_name=crunge
+repo_crunge_type=user
 
-user_module_git_link=(gitconfig)
-#user_module_git_template=(gitconfig)
+crunge_module_bash_from=shells/bash/
+crunge_module_bash_link=(bash_logout bash_profile bashrc)
+crunge_module_bash_depend=(shell)
 
-user_module_i3_from=x/
-user_module_i3_link=(i3)
-user_module_i3_depend=(x)
+crunge_module_git_link=(gitconfig)
+#crunge_module_git_template=(gitconfig)
 
-user_module_nvim_to=.config
-user_module_nvim_link=(nvim)
-user_module_nvim_options=(noprefix)
+crunge_module_i3_from=x/
+crunge_module_i3_link=(i3)
+crunge_module_i3_depend=(x)
 
-user_module_oni_link=(oni)
-user_module_oni_depend=(nvim)
+crunge_module_nvim_to=.config
+crunge_module_nvim_link=(nvim)
+crunge_module_nvim_options=(noprefix)
 
-user_module_shell_from=shells/
-user_module_shell_link=(shellrc)
+crunge_module_oni_link=(oni)
+crunge_module_oni_depend=(nvim)
 
-user_module_vim_link=(vim vimrc gvimrc)
+crunge_module_shell_from=shells/
+crunge_module_shell_link=(shellrc)
 
-user_module_x_from=x/
-user_module_x_link=(xinitrc Xresources xprofile)
+crunge_module_vim_link=(vim vimrc gvimrc)
 
-user_module_xscreensaver_from=x/
-user_module_xscreensaver_link=(xscreensaver)
-user_module_xscreensaver_depend=(x)
+crunge_module_x_from=x/
+crunge_module_x_link=(xinitrc Xresources xprofile)
 
-user_module_xdg_to=.config
-user_module_xdg_link=(user-dirs.dirs)
-user_module_xdg_options=(noprefix)
+crunge_module_xscreensaver_from=x/
+crunge_module_xscreensaver_link=(xscreensaver)
+crunge_module_xscreensaver_depend=(x)
 
-user_module_zsh_from=shells/
-user_module_zsh_link=(zshrc)
-user_module_zsh_depend=(shell)
+crunge_module_xdg_to=.config
+crunge_module_xdg_link=(user-dirs.dirs)
+crunge_module_xdg_options=(noprefix)
+
+crunge_module_zsh_from=shells/
+crunge_module_zsh_link=(zshrc)
+crunge_module_zsh_depend=(shell)
 
